2000630120f30104a9279e0c6b124b03bf082537
basem-atia <basematiaelsayed@gmail.com>
2025-01-23 19:52:25 +0200

    second edit basem

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 console.log("root");
 const substract = (x, y) => x - y;
 
-
 function add(a, b) {
   return a + b;
 }
 function product(a, b) {
   return a * b;
-
 }
+console.log(add(1 + 2), substract(9, 6), product(5, 3));
