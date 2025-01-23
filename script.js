178591bda045e443d1165e21c3d0456afabf131d
MohamedEl-Tabei <mohamedeltabei2000@gmail.com>
2025-01-23 19:52:27 +0200

    x+y+z

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 console.log("root");
 const substract = (x, y) => x - y;
 
-
 function add(a, b) {
   return a + b;
 }
 function product(a, b) {
   return a * b;
-
 }
+let x = add(2, 5);
+let y = substract(15, 10);
+let z = product(8, 5);
+
+console.log(x + y + z);
