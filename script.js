0f2691dffa98993ab5f70654bc28a74b0d967a29
karim mohamed <karimhelmi4@mail.com>
2025-01-23 19:52:30 +0200

    edit

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 console.log("root");
 const substract = (x, y) => x - y;
 
-
 function add(a, b) {
   return a + b;
 }
 function product(a, b) {
   return a * b;
-
 }
+let x = 10;
+let y = 20;
+console.log(add(x, y));
+console.log(product(x, y));
+console.log(substract(x, y));
